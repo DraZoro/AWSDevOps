d76cecc4cbce030dcb2e9782cd886a32c23551e7
Clement Mahlangu <clement.mahlangu@gmail.com>
2022-11-18 03:00:55 +0200

    Enable S3 backent for state files

--- development_tools/terraform.tf
+++ development_tools/terraform.tf
@@ -6,12 +6,11 @@ terraform {
     }
   }
 
-  //Todo: Introduce remote state
-  # required_version = ">= 1.2.7"
-  # backend "s3" {
-  #   bucket = "draznet-infra"
-  #   key    = "tf-states/codetools.tfstate"
-  #   region = "af-south-1"
-  # }
+  required_version = ">= 1.3.5"
+  backend "s3" {
+    bucket = "draznet-infra"
+    key    = "tf-states/codetools.tfstate"
+    region = "af-south-1"
+  }
 
 }
