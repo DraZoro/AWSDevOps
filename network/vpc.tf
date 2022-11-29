f36c90aac1c055a0e54faec763328f0109c25ed3
Clement Mahlangu <clement.mahlangu@gmail.com>
2022-11-29 16:46:59 +0200

    Associated public route with IGW

--- network/vpc.tf
+++ network/vpc.tf
@@ -1,19 +1,19 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "cka-training"
+    Name    = "cka-training"
     Project = "Training"
   }
 }
 
 # Subnets 
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "af-south-1a"
 
   tags = {
@@ -22,9 +22,9 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-   availability_zone = "af-south-1a"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "af-south-1a"
 
 
   tags = {
@@ -44,6 +44,11 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
   tags = {
     Name = "Public"
